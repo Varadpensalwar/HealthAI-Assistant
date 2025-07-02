5f5fce037b774e58d28c84df3eba70090533fbd5
Varad Pensalwar <varadpensalwar@gmail.com>
2025-07-02 15:31:47 +0530

    folder structure added

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'HealthAI-Assistant',
+    version= '0.0.0',
+    author= 'Varad Pensalwar',
+    author_email= 'varadpensalwar@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
